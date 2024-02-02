16f302ae10fd9b1d33dd7e4f49618a09c51e50c6
Cian-McKelvey <cian.p.mckelvey@gmail.com>
2024-02-02 11:11:28 +0000

    Changing part calculations plan

--- pc_builder_backend/part_calculations.py
+++ pc_builder_backend/part_calculations.py
@@ -1,69 +1,16 @@
 # Ripped from the existing GitHub repo, ignore this it probably sucks
 # Collection of methods that allow the calculation of approximate part costs
 
-
-def cpu_cost_calculator(budget):
-    if budget <= 1000:
-        return (budget / 100) * 30  # The 30 here is the percentage. This is not a final number
-    if budget > 1000 & budget <= 1500:
-        return 2
-    if budget > 1500 & budget <= 2000:
-        return 3
-    if budget > 2000:
-        return 4
+"""
+    Note that due to the nature of not knowing compatibility, we will not be showing any fans, or other misc elements
+    These will be up to the users discretion
+"""
 
 
-def gpu_cost_calculator(budget):
-    if budget <= 1000:
-        return 1
-    if budget > 1000 & budget <= 1500:
-        return 2
-    if budget > 1500 & budget <= 2000:
-        return 3
-    if budget > 2000:
-        return 4
+# Generates a budget percentage for each component
+def build_cost_calculation() -> dict:
+    budget_dict = {}
 
+    # If budget is under 500, append these prices etc...
 
-def motherboard_cost_calculator(budget):
-    if budget <= 1000:
-        return 1
-    if budget > 1000 & budget <= 1500:
-        return 2
-    if budget > 1500 & budget <= 2000:
-        return 3
-    if budget > 2000:
-        return 4
-
-
-def storage_cost_calculator(budget):
-    if budget <= 1000:
-        return 1
-    if budget > 1000 & budget <= 1500:
-        return 2
-    if budget > 1500 & budget <= 2000:
-        return 3
-    if budget > 2000:
-        return 4
-
-
-def power_supply_cost_calculator(budget):
-    if budget <= 1000:
-        return 1
-    if budget > 1000 & budget <= 1500:
-        return 2
-    if budget > 1500 & budget <= 2000:
-        return 3
-    if budget > 2000:
-        return 4
-
-
-def ram_cost_calculator(budget):
-    if budget <= 1000:
-        return 1
-    if budget > 1000 & budget <= 1500:
-        return 2
-    if budget > 1500 & budget <= 2000:
-        return 3
-    if budget > 2000:
-        return 4
-
+    return budget_dict
