c2654a7f476bfdddbff58e2b2569a501ca6ba217
Cian-McKelvey <cian.p.mckelvey@gmail.com>
2024-02-08 12:14:14 +0000

    More messing about with the excel stuff

--- pc_builder_backend/excel_methods/excel_helper_methods.py
+++ pc_builder_backend/excel_methods/excel_helper_methods.py
@@ -1,19 +1,28 @@
+from typing import Union
+
 import pandas as pd
-
-# This definitely sucks too but there's nothing here so who cares
-
-
-# Used for dict's, lists etc
-def write_to_excel(filepath, data):
-    df = pd.DataFrame(data)
-    df.to_excel(filepath, sheet_name='Sheet1', index=False)
-
-
-# Used for pre-made dataframes
-def write_frame_to_excel(filepath, frame):
-    frame.to_excel(filepath, sheet_name='Sheet1', index=False)
 
 
 def read_excel_data(filepath: str) -> pd.DataFrame:
     dataframe = pd.read_excel(filepath)
     return dataframe
+
+
+def fetch_valid_parts(part_name: str, parts_dataframe: pd.DataFrame, target_price: Union[int, float]) -> pd.DataFrame:
+    part_type = part_name
+    target_plus = target_price + (target_price * 15 / 100)
+    target_minus = target_price - (target_price * 15 / 100)
+    query_string = '(Type == @part_type) & (@target_minus <= Price <= @target_plus)'
+    trimmed_dataframe = parts_dataframe.query(query_string)
+    return trimmed_dataframe
+
+
+"""
+target_price = 70
+target_plus = target_price + (target_price * 15 / 100)
+target_minus = target_price - (target_price * 15 / 100)
+
+# Filter the DataFrame to only include SSD parts with prices between target_minus and target_plus
+ssd_dataframe = complete_parts_df.query('(Type == "SSD") & (@target_minus <= Price <= @target_plus)')
+print(ssd_dataframe)
+"""
--- pc_builder_backend/excel_methods/excel_testing.py
+++ pc_builder_backend/excel_methods/excel_testing.py
@@ -1,4 +1,4 @@
-from excel_helper_methods import read_excel_data
+from excel_helper_methods import read_excel_data, fetch_valid_parts
 import os
 import pandas as pd
 
@@ -18,6 +18,19 @@ print(cpu_dataframe)
 
 print("\n\n\n\n")
 
-# Filter the DataFrame to only include CPU parts
-ssd_dataframe = complete_parts_df.query('Type == "SSD"')
+# Target price and adjustment values, gets the aimed price of the part in this case its hardcoded.
+# Adds 15% to use as the higher range, subtracts 15% to use as the lower range
+target_price = 70
+target_plus = target_price + (target_price * 15 / 100)
+target_minus = target_price - (target_price * 15 / 100)
+
+# Filter the DataFrame to only include SSD parts with prices between target_minus and target_plus
+ssd_dataframe = complete_parts_df.query('(Type == "SSD") & (@target_minus <= Price <= @target_plus)')
 print(ssd_dataframe)
+
+
+print("-------------------")
+
+
+method_ssd_dataframe = fetch_valid_parts(part_name="SSD", parts_dataframe=complete_parts_df, target_price=70)
+print(method_ssd_dataframe)
