6b2ecb7b61115b69b65fc29970ffbedc620bbf38
Cian-McKelvey <cian.p.mckelvey@gmail.com>
2024-02-26 22:54:37 +0000

    Added the ability to pull parts for every data type, still experimenting with crawling

--- pc_builder_backend/webscraping/crawler.py
+++ pc_builder_backend/webscraping/crawler.py
@@ -11,17 +11,27 @@ def fetch_links(url) -> list:
     return absolute_links
 
 
-def fetch_links_with_page_mentioned(url) -> list:
-    links = []
+def fetch_links_with_page_mentioned_old(url) -> list:
+    links = set()
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     for link in soup.find_all('a', href=True):
-        if 'page' in link['href']:
-            links.append(link['href'])
-    return links
+        if 'page' in link['href']:  # Fetches only the links with mentions of a page number
+            links.add(link['href'])
+    return list(links)
 
 
-url = 'https://pcparts.uk/browse/cpus'
-links = fetch_links_with_page_mentioned(url)
-for link in links:
-    print(link)
+def fetch_links_with_page_mentioned(url) -> list:
+    links = set()  # Use a set to automatically remove duplicates
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for HTTP errors (e.g., 404)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        base_url = response.url  # Get the base URL in case the links are relative
+        for link in soup.find_all('a', href=True):
+            absolute_url = urljoin(base_url, link['href'])  # Convert relative URL to absolute URL
+            if 'page' in absolute_url:  # Check if the absolute URL contains 'page'
+                links.add(absolute_url)  # Add the absolute URL to the set
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching links from {url}: {e}")
+    return list(links)  # Convert the set back to a list before returning it
--- pc_builder_backend/webscraping/part_data_scraper.py
+++ pc_builder_backend/webscraping/part_data_scraper.py
@@ -1,13 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-from crawler import fetch_links_with_page_mentioned
 
 
-def extract_cpu_info(html_content):
+def scrape_part_data(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     products = soup.find_all('div', class_='card-horizontal')
 
-    cpu_info = {}
+    part_info = {}
     for product in products:
         name = product.find('div', class_='title').text.strip()
         price_elem = product.find('div', class_='price in-stock')
@@ -15,50 +14,9 @@ def extract_cpu_info(html_content):
             price = price_elem.text.strip()
         else:
             price = 'Price not available'
-        cpu_info[name] = price
+        part_info[name] = price
 
-    return cpu_info
-
-
-def extract_gpu_info(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    products = soup.find_all('div', class_='card-horizontal')
-
-    gpu_info = {}
-    for product in products:
-        name = product.find('div', class_='title').text.strip()
-        price_elem = product.find('div', class_='price in-stock')
-        if price_elem:
-            price = price_elem.text.strip()
-        else:
-            price = 'Price not available'
-        gpu_info[name] = price
-
-    return gpu_info
-
-
-def extract_ram_info(html_content):
-    ...
-
-
-def extract_ssd_info(html_content):
-    ...
-
-
-def extract_hdd_info(html_content):
-    ...
-
-
-def extract_motherboard_info(html_content):
-    ...
-
-
-def extract_power_supply_info(html_content):
-    ...
-
-
-def extract_case_info(html_content):
-    ...
+    return part_info
 
 
 def fetch_html_content(url):
@@ -73,43 +31,201 @@ def fetch_html_content(url):
 def main():
     cpu_url = "https://pcparts.uk/browse/cpus"
     gpu_url = "https://pcparts.uk/browse/graphics-cards"
+    ram_url = "https://pcparts.uk/browse/memory"
+    ssd_url = "https://pcparts.uk/browse/solid-state-drives"
+    hdd_url = "https://pcparts.uk/browse/hard-drives"
+    motherboard_url = "https://pcparts.uk/browse/motherboards"
+    power_supply_url = "https://pcparts.uk/browse/power-supplies"
+    case_url = "https://pcparts.uk/browse/cases"
 
-    #url_list = []
-    #url_list.append(url)
-    #fetched_links = fetch_links_with_page_mentioned(url)
-    # Append the second item onwards from fetched_links to url_list
-    #url_list.extend(fetched_links[1:])
+    # Fetch all the urls for the parts
+    cpu_storage_dict = {}
+    cpu_url_list = [cpu_url]
+    # fetched_cpu_urls = fetch_links_with_page_mentioned_old(cpu_url)
+    # cpu_url_list.append(fetched_cpu_urls[1:])
 
-    # cpu_dict = {}
-    # gpu_dict = {}
+    gpu_storage_dict = {}
+    gpu_url_list = [gpu_url]
+    # fetched_gpu_urls = fetch_links_with_page_mentioned(gpu_url)
+    # gpu_url_list.append(fetched_gpu_urls[2:])
 
-    cpu_content = fetch_html_content(cpu_url)
-    gpu_content = fetch_html_content(gpu_url)
+    ram_storage_dict = {}
+    ram_url_list = [ram_url]
+    # fetched_ram_urls = fetch_links_with_page_mentioned(ram_url)
+    # ram_url_list.append(fetched_ram_urls[2:])
 
-    cpu_dict = extract_cpu_info(html_content=cpu_content)
-    gpu_dict = extract_gpu_info(html_content=gpu_content)
+    ssd_storage_dict = {}
+    ssd_url_list = [ssd_url]
+    # fetched_ssd_urls = fetch_links_with_page_mentioned(ssd_url)
+    # ssd_url_list.append(fetched_ssd_urls[2:])
 
-    # for url in url_list:
-    #
-    #     html_content = fetch_html_content(url)
-    #
-    #     if html_content:
-    #         cpu_info = extract_cpu_info(html_content)
-    #         if cpu_info:
-    #
-    #             cpu_dict.update(cpu_info)
-    #
-    #             for name, price in cpu_info.items():
-    #                 print("Name:", name)
-    #                 print("Price:", price)
-    #                 print()
-    #         else:
-    #             print("No CPU information found on the page.")
-    #     else:
-    #         print("Unable to fetch data from the provided URL.")
+    hdd_storage_dict = {}
+    hdd_url_list = [hdd_url]
+    # fetched_hdd_urls = fetch_links_with_page_mentioned(hdd_url)
+    # hdd_url_list.append(fetched_hdd_urls[2:])
 
-    print(cpu_dict)
-    print(gpu_dict)
+    motherboard_storage_dict = {}
+    motherboard_url_list = [motherboard_url]
+    # fetched_motherboard_urls = fetch_links_with_page_mentioned(motherboard_url)
+    # motherboard_url_list.append(fetched_motherboard_urls[2:])
+
+    power_supply_storage_dict = {}
+    power_supply_url_list = [power_supply_url]
+    # fetched_power_supply_urls = fetch_links_with_page_mentioned(power_supply_url)
+    # power_supply_url_list.append(fetched_power_supply_urls[2:])
+
+    case_storage_dict = {}
+    case_url_list = [case_url]
+    # fetched_case_urls = fetch_links_with_page_mentioned(case_url)
+    # case_url_list.append(fetched_case_urls[2:])
+
+    for url in cpu_url_list:
+        html_data = fetch_html_content(url)
+
+        if html_data:
+            cpu_data = scrape_part_data(html_content=html_data)
+
+            if cpu_data:
+                cpu_storage_dict.update(cpu_data)
+            else:
+                print("NO CPU DATA FOUND")
+        else:
+            print("URL ISN'T VALID + ", url)
+
+    for url in gpu_url_list:
+        html_data = fetch_html_content(url)
+
+        if html_data:
+            gpu_data = scrape_part_data(html_content=html_data)
+
+            if gpu_data:
+                gpu_storage_dict.update(gpu_data)
+            else:
+                print("NO CPU DATA FOUND")
+        else:
+            print("URL ISN'T VALID + ", url)
+
+
+    for url in ram_url_list:
+        html_data = fetch_html_content(url)
+
+        if html_data:
+            ram_data = scrape_part_data(html_content=html_data)
+
+            if ram_data:
+                ram_storage_dict.update(ram_data)
+            else:
+                print("NO CPU DATA FOUND")
+        else:
+            print("URL ISN'T VALID + ", url)
+
+    # Loop for scraping data from HDD URLs
+    for url in hdd_url_list:
+        html_data = fetch_html_content(url)
+
+        if html_data:
+            hdd_data = scrape_part_data(html_content=html_data)
+
+            if hdd_data:
+                hdd_storage_dict.update(hdd_data)
+            else:
+                print("NO HDD DATA FOUND")
+        else:
+            print("URL ISN'T VALID: ", url)
+
+    # Loop for scraping data from SSD URLs
+    for url in ssd_url_list:
+        html_data = fetch_html_content(url)
+
+        if html_data:
+            ssd_data = scrape_part_data(html_content=html_data)
+
+            if ssd_data:
+                ssd_storage_dict.update(ssd_data)
+            else:
+                print("NO SSD DATA FOUND")
+        else:
+            print("URL ISN'T VALID: ", url)
+
+    # Loop for scraping data from Motherboard URLs
+    for url in motherboard_url_list:
+        html_data = fetch_html_content(url)
+
+        if html_data:
+            motherboard_data = scrape_part_data(html_content=html_data)
+
+            if motherboard_data:
+                motherboard_storage_dict.update(motherboard_data)
+            else:
+                print("NO MOTHERBOARD DATA FOUND")
+        else:
+            print("URL ISN'T VALID: ", url)
+
+    # Loop for scraping data from Power Supply URLs
+    for url in power_supply_url_list:
+        html_data = fetch_html_content(url)
+
+        if html_data:
+            power_supply_data = scrape_part_data(html_content=html_data)
+
+            if power_supply_data:
+                power_supply_storage_dict.update(power_supply_data)
+            else:
+                print("NO POWER SUPPLY DATA FOUND")
+        else:
+            print("URL ISN'T VALID: ", url)
+
+    # Loop for scraping data from Case URLs
+    for url in case_url_list:
+        html_data = fetch_html_content(url)
+
+        if html_data:
+            case_data = scrape_part_data(html_content=html_data)
+
+            if case_data:
+                case_storage_dict.update(case_data)
+            else:
+                print("NO CASE DATA FOUND")
+        else:
+            print("URL ISN'T VALID: ", url)
+
+    # Printing to test
+    print(len(cpu_storage_dict))
+    print(cpu_storage_dict)
+    print("\n")
+
+    print(len(gpu_storage_dict))
+    print(gpu_storage_dict)
+    print("\n")
+
+    print(len(ram_storage_dict))
+    print(ram_storage_dict)
+    print("\n")
+
+    print("HDD Data:")
+    print(len(hdd_storage_dict))
+    print(hdd_storage_dict)
+    print("\n")
+
+    print("SSD Data:")
+    print(len(ssd_storage_dict))
+    print(ssd_storage_dict)
+    print("\n")
+
+    print("Motherboard Data:")
+    print(len(motherboard_storage_dict))
+    print(motherboard_storage_dict)
+    print("\n")
+
+    print("Power Supply Data:")
+    print(len(power_supply_storage_dict))
+    print(power_supply_storage_dict)
+    print("\n")
+
+    print("Case Data:")
+    print(len(case_storage_dict))
+    print(case_storage_dict)
+    print("\n")
 
 
 if __name__ == "__main__":
