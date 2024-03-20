59fe5d3135c4bb43031f134dd2d75646ba30d170
Cian-McKelvey <cian.p.mckelvey@gmail.com>
2024-03-20 19:45:14 +0000

    Added test file example

---
+++ pc_builder_backend/test/test_excel_helper_methods.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestExcelHelperMethods(unittest.TestCase):
+
+    def test_functionality(self):
+        i = 1
+        self.assertEqual(i, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
